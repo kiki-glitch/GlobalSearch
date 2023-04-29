890f89c74e5ac8ba4cab73edc99cfb2d83dedd70
unknown <alex.wamai@strathmore.edu>
2023-04-29 13:38:55 +0300

    Small Change

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -251,7 +251,7 @@ Nora Silvester
 Anything you want
 </div>
 
-<strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
+<strong>Copyright &copy; {{ date('Y') }} <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
 </footer>
 </div>
 
